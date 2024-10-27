d7dfb092ac1e25fe2fd3bfafcbe85037dd43e3a8
Bracha Rosenfeld <bracharosenfeld@gmail.com>
2024-10-27 21:44:15 +0200

    "Initial Code + Welcome functions".

---
+++ Stage0/Program1172.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome1172();
+            Welcome5853();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome5853();
+        private static void Welcome1172()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"{name}, welcome to my first console application");
+        }
+    }
+}
