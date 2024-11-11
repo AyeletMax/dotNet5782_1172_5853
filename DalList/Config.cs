fa0ec29b68bc7a9c68cec01095fce0d76e5243b3
Bracha Rosenfeld <bracharosenfeld@gmail.com>
2024-11-11 23:18:52 +0200

    comited all

---
+++ DalList/Config.cs
@@ -0,0 +1,20 @@
+﻿namespace Dal;
+
+internal static class Config
+{
+    internal const int StartId = 1;
+    private static int id = StartId;
+    internal static int Id { get => id++; }
+    //...
+
+    internal static DateTime Clock { get; set; } = DateTime.Now;
+    //...
+
+    internal static void Reset()
+    {
+       
+        //...
+        Clock = DateTime.Now;
+        //...
+    }
+}
