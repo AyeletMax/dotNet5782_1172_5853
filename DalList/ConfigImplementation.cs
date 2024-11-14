6ea29a01752578b7ebe9f6f2063512d5cd439537
AyeletMax <Hand4d20@mbjcomp.org.il>
2024-11-14 23:46:31 +0200

    I did ConfigImplementation

---
+++ DalList/ConfigImplementation.cs
@@ -0,0 +1,17 @@
+﻿using DalApi;
+
+namespace Dal;
+
+public class ConfigImplementation : IConfig
+{
+    public DateTime Clock
+    {
+        get => Config.Clock;
+        set => Config.Clock = value;
+    }
+    public void Reset()
+    {
+        Config.Reset();
+    }
+
+}
