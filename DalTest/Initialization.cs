995194bb72591365d5026c16da813d4ad3616507
Bracha Rosenfeld <bracharosenfeld@gmail.com>
2024-11-19 22:10:58 +0200

    a new class - Initialization

---
+++ DalTest/Initialization.cs
@@ -0,0 +1,12 @@
+﻿namespace DalTest;
+using DalApi;
+using DO;
+public static class Initialization
+{
+    private static IAssignment? s_dalAssignment;
+    private static ICall? s_dalCall;
+    private static IConfig? s_dalConfig;
+    private static IVolunteer? s_dalVolunteer;
+
+    private static readonly Random s_rand = new();
+}
