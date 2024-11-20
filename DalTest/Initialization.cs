8ab56bb04f0164008e120ae7072d89a2b9d19e74
AyeletMax <Hand4d20@mbjcomp.org.il>
2024-11-20 20:43:57 +0200

    I created all entity

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -9,4 +9,24 @@ public static class Initialization
     private static IVolunteer? s_dalVolunteer;
 
     private static readonly Random s_rand = new();
+    private static void createVolunteer()
+    {
+        string[] volunteerNames =
+       { "Dani Levy", "Eli Amar", "Yair Cohen", "Ariela Levin", "Dina Klein", "Shira Israelof" };
+        foreach (var name in volunteerNames)
+        {
+            int id;
+            do
+                id = s_rand.Next(200000000, 400000000);
+            while (s_dalVolunteer!.Read(id) != null);
+
+            string[] fullName = name.Split(' ');
+            string FirstName = fullName[0];
+            string LastName = fullName[1];
+
+
+
+        }
+
+    }
 }
