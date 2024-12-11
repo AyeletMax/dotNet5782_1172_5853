a6b19d21b316c6b6cc692fddf484dc413ad0ffc1
Bracha Rosenfeld <bracharosenfeld@gmail.com>
2024-12-11 21:28:39 +0200

    started moving to xml

---
+++ DalXml/AssignmentImplementation.cs
@@ -0,0 +1,58 @@
+﻿namespace Dal;
+using DalApi;
+using DO;
+using System;
+using System.Collections.Generic;
+
+internal class AssignmentImplementation : IAssignment
+{
+    public void Create(Assignment item)
+    {
+        int newId = Config.NextAssignmentId;
+        List<Assignment> assignments = XMLTools.LoadListFromXMLSerializer<Assignment>(Config.s_assignments_xml);
+        Assignment newAssignment = item with { Id = newId };
+        assignments.Add(newAssignment);
+        XMLTools.SaveListToXMLSerializer(assignments, Config.s_assignments_xml);
+    }
+
+    //צריך לשאול מישהי על הזריקות פה
+    public void Delete(int id)
+    {
+    
+    }
+
+    public void DeleteAll()
+    {
+
+    }
+
+    public Assignment? Read(int id)
+    {
+        List<Assignment> assignments = XMLTools.LoadListFromXMLSerializer<Assignment>(Config.s_assignments_xml);
+
+        return assignments.FirstOrDefault(item => item.Id == id);
+    }
+
+    public Assignment? Read(Func<Assignment, bool> filter)
+    {
+        List<Assignment> assignments = XMLTools.LoadListFromXMLSerializer<Assignment>(Config.s_assignments_xml);
+        return assignments.FirstOrDefault(filter);
+    }
+
+    public IEnumerable<Assignment> ReadAll(Func<Assignment, bool>? filter = null)
+    {
+        List<Assignment> assignments = XMLTools.LoadListFromXMLSerializer<Assignment>(Config.s_assignments_xml);
+        return filter == null
+            ? assignments
+            : assignments.Where(filter);
+    }
+
+    public void Update(Assignment item)
+    {
+        List<Assignment> assignments = XMLTools.LoadListFromXMLSerializer<Assignment>(Config.s_assignments_xml);
+        if (assignments.RemoveAll(it => it.Id == item.Id) == 0)
+            throw new DalDoesNotExistException($"Assignment with ID={item.Id} does Not exist");
+        assignments.Add(item);
+        XMLTools.SaveListToXMLSerializer(assignments, Config.s_assignments_xml);
+    }
+}
