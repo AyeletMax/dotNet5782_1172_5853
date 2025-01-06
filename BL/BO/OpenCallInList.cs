176f233389a263baaba8818f264cdfe8d81afa79
Ayelet Maximove <maximove@acad.jct.ac.il>
2025-01-06 21:26:39 +0200

    עשיתי את כל הישויות שנשארו גמרנו להגדיר ת הישויות

--- BL/BO/OpenCallInList.cs
+++ BL/BO/OpenCallInList.cs
@@ -4,4 +4,11 @@ namespace BO;
 
 public class OpenCallInList
 {
+    public int Id { get; init; }
+    public CallType MyCallType { get; set; }
+    public string? VerbalDescription { get; set; }
+    public string Address { get; set; }
+    public DateTime OpenTime { get; set; }
+   public DateTime? MaxFinishTime { get; set; }
+    public double distanceFromVolunteerToCall {  get; set; }
 }
