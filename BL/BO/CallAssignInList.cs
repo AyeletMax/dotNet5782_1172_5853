04879fe107d13a48977911e0e6e87e1aa2c8b768
Ayelet Maximove <maximove@acad.jct.ac.il>
2025-01-06 20:50:25 +0200

    עשיתי את הישות CallAssignInList

--- BL/BO/CallAssignInList.cs
+++ BL/BO/CallAssignInList.cs
@@ -4,4 +4,10 @@ namespace BO;
 
 public class CallAssignInList
 {
+    public int? VolunteerId {  get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime EntranceTime {  get; set; }
+    public DateTime? ExitTime {  get; set; }
+    public FinishCallType? FinishCallType {  get; set; }
 }
