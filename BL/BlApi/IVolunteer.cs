19a23894f1d1d1f58be1f0b88deda7834157e874
AyeletMax <Hand4d20@mbjcomp.org.il>
2025-01-09 22:27:31 +0200

    תיקון הרשימה לIEnumerable

--- BL/BlApi/IVolunteer.cs
+++ BL/BlApi/IVolunteer.cs
@@ -3,7 +3,7 @@ using BO;
 public interface IVolunteer
 {
     Role Login(string name, string password);
-    List<BO.VolunteerInList> GetVolunteersList(bool? isActive,  CallType? callType);
+    IEnumerable<BO.VolunteerInList> GetVolunteersList(bool? isActive,  CallType? callType);
     BO.Volunteer GetVolunteerDetails(int volunteerId);
     void UpdateVolunteerDetails(int requesterId, BO.Volunteer volunteer);
     void DeleteVolunteer(int volunteerId);
