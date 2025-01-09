eb7bd0931b91bccd66acbccc1095c9988533f7fc
Bracha Rosenfeld <bracharosenfeld@gmail.com>
2025-01-09 22:32:58 +0200

    updated IAdmin

--- BL/BlApi/IAdmin.cs
+++ BL/BlApi/IAdmin.cs
@@ -3,10 +3,10 @@ namespace BlApi;
 
 public interface IAdmin
 {
+    void InitializeDB();
+    void ResetDB();
+    int GetMaxRange();
+    void SetMaxRange(int maxRange);
     DateTime GetClock();
     void AdvanceClock(BO.TimeUnit unit);
-    TimeSpan GetRiskTimeSpan();
-    void SetRiskTimeSpan(TimeSpan riskTimeSpan);
-    void ResetDatabase();
-    void InitializeDatabase();
 }
