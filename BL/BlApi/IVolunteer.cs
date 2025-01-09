ae7f855f9e9dd7fb1245b92d3bf0861a982b3d66
Bracha Rosenfeld <bracharosenfeld@gmail.com>
2025-01-09 22:16:33 +0200

    declarations for IVolunteer functions

--- BL/BlApi/IVolunteer.cs
+++ BL/BlApi/IVolunteer.cs
@@ -3,7 +3,11 @@ using BO;
 public interface IVolunteer
 {
     Role Login(string name, string password);
-    
+    List<BO.VolunteerInList> GetVolunteersList(bool? isActive,  CallType? callType);
+    BO.Volunteer GetVolunteerDetails(int volunteerId);
+    void UpdateVolunteerDetails(int requesterId, BO.Volunteer volunteer);
+    void DeleteVolunteer(int volunteerId);
+    void AddVolunteer(BO.Volunteer volunteer);
 }
 
 
