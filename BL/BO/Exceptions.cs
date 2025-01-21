66a2303bff35e58ed158376cf26c20374b9357a8
Ayelet Maximove <maximove@acad.jct.ac.il>
2025-01-21 14:12:45 +0200

    עוד חריגה נוספה

--- BL/BO/Exceptions.cs
+++ BL/BO/Exceptions.cs
@@ -9,4 +9,11 @@ public class GeneralDatabaseException : Exception
 {
     public GeneralDatabaseException(string? message, Exception? innerException) : base(message) { }
 }
+public class DalDoesNotExistException : Exception
+{
+    public DalDoesNotExistException(string? message) : base(message)
+    {
+
+    }
+}
 
