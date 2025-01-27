a889d6e96bab1541c602b4dd51ee203119b91bc6
Bracha Rosenfeld <bracharosenfeld@gmail.com>
2025-01-27 19:51:19 +0200

    another small change to the exception

--- BL/BO/Exceptions.cs
+++ BL/BO/Exceptions.cs
@@ -11,65 +11,38 @@ public class GeneralDatabaseException : Exception
 }
 public class BLDoesNotExistException : Exception
 {
-    public BLDoesNotExistException(string? message, Exception ex) : base(message)
-    {
-
-    }
+    public BLDoesNotExistException(string? message, Exception ex) : base(message) { }
 }
 
 public class GeolocationNotFoundException : Exception
 {
-    public GeolocationNotFoundException(string? message) : base(message)
-    {
-
-    }
+    public GeolocationNotFoundException(string? message) : base(message) { }
 }
 public class BlDoesNotExistn : Exception
 {
-    public BlDoesNotExistn(string? message) : base(message)
-    {
-
-    }
+    public BlDoesNotExistn(string? message) : base(message) { }
 }
 public class InvalidFormatException : Exception
 {
-    public InvalidFormatException(string? message) : base(message)
-    {
-
-    }
+    public InvalidFormatException(string? message) : base(message) { }
 }
 public class DeletionException : Exception
 {
-    public DeletionException(string? message) : base(message)
-    {
-
-    }
+    public DeletionException(string? message) : base(message) { }
 }
 public class BLDoesNotExist : Exception
 {
-    public BLDoesNotExist(string? message, Exception ex) : base(message)
-    {
-
-    }
+    public BLDoesNotExist(string? message, Exception ex) : base(message) { }    
 }
 public class AssignmentNotFoundException : Exception
 {
-    public AssignmentNotFoundException(string? message) : base(message)
-    {
-
-    }
+    public AssignmentNotFoundException(string? message) : base(message) { }    
 }
 public class PermissionDeniedException : Exception
 {
-    public PermissionDeniedException(string? message) : base(message)
-    {
-
-    }
+    public PermissionDeniedException(string? message) : base(message)    { }
 }
 public class InvalidOperationException : Exception
 {
-    public InvalidOperationException(string? message) : base(message)
-    {
-
-    }
+    public InvalidOperationException(string? message) : base(message)    { }
 }
