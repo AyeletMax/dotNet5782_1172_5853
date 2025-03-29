4b387b8aba0f4fb6150f8d9fd5f91abe1c81c9a7
Bracha Rosenfeld <bracharosenfeld@gmail.com>
2025-03-30 00:40:31 +0300

    I added documentation for a buch of files

--- BL/BO/VolunteerInList.cs
+++ BL/BO/VolunteerInList.cs
@@ -5,14 +5,23 @@ namespace BO;
 
 public class VolunteerInList
 {
+    /// The unique identifier of the volunteer.
     public int Id { get; init; }
+    /// The name of the volunteer.
     public string Name { get; init; }
+    /// Indicates whether the volunteer is active.
     public bool Active { get; init; }
+    /// The total number of responses handled by the volunteer.
     public int TotalResponsesHandled { get; init; }
+    /// The total number of responses canceled by the volunteer.
     public int TotalResponsesCancelled { get; init; }
+    /// The total number of responses that expired.
     public int TotalExpiredResponses { get; init; }
+    /// The ID of the response currently assigned to the volunteer (if any).
     public int? AssignedResponseId { get; init; }
+    /// The type of call the volunteer handles.
     public CallType MyCallType {  get; init; }
+    /// Returns a string representation of the object.
     public override string ToString() => this.ToStringProperty();
 }
 
--- BL/Helpers/AssignmentManager.cs
+++ BL/Helpers/AssignmentManager.cs
@@ -3,8 +3,11 @@
 using DalApi;
 
 namespace Helpers;
-
+/// <summary>
+/// A static helper class for managing assignments.
+/// </summary>
 internal static class AssignmentManager
 {
+    /// An instance of the data access layer (DAL).
     private static IDal s_dal = Factory.Get; //stage 4
 }
