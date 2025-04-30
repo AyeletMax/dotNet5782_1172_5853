f27dcd8bc7212667fd203800497e39826bae15ee
Bracha Rosenfeld <bracharosenfeld@gmail.com>
2025-04-30 20:17:06 +0300

    started sub chapter 8 first part - with the Enums

--- PL/Enums.cs
+++ PL/Enums.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,16 @@ using System.Threading.Tasks;
 
 namespace PL;
 
-internal class Enums
+internal class VolunteerSortFieldCollection : IEnumerable
 {
+    static readonly IEnumerable<BO.VolunteerSortField> s_enums =
+        (System.Enum.GetValues(typeof(BO.VolunteerSortField)) as IEnumerable<BO.VolunteerSortField>)!;
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
+
+internal class CallInListFieldsCollection : IEnumerable
+{
+    static readonly IEnumerable<BO.CallInListFields> s_enums =
+        (System.Enum.GetValues(typeof(BO.CallInListFields)) as IEnumerable<BO.CallInListFields>)!;
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
 }
