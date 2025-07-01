acebb6225ae82cfda4e87db93eb1b3ae4f0e3ea3
Bracha-Rosenfeld <Hand4d28@mbjcomp.org.il>
2025-07-02 00:39:22 +0300

    I fixed the issue with the active button on the volunteer window - updating active or not is only allowed if no call allocated to volunteer

--- PL/Converters/NullToBoolConverter.cs
+++ PL/Converters/NullToBoolConverter.cs
@@ -7,14 +7,30 @@ using System.Threading.Tasks;
 using System.Windows.Data;
 
 namespace PL.Converters;
+//public class NullToBoolConverter : IValueConverter
+//{
+//    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+//    {
+//        // אם null -> false, אחרת true
+//        return value != null;
+//    }
+
+//    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+//        throw new NotImplementedException();
+//}
 public class NullToBoolConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        // אם null -> false, אחרת true
-        return value != null;
+        bool result = value != null;
+
+        if (parameter?.ToString()?.ToLower() == "invert")
+            result = !result;
+
+        return result;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
         throw new NotImplementedException();
 }
+
